dd66fcc1e7cf332abfcff99c1af6003590f8d005
Marlon Smith <msmith@kcura.com>
2018-04-11 11:17:32 -0500

    Updating Event Handler Templates

--- Source/Code/Relativity Project Templates/EventHandler Project Templates/Relativity ConsoleEventHandler/ConsoleEventHandler.cs
+++ Source/Code/Relativity Project Templates/EventHandler Project Templates/Relativity ConsoleEventHandler/ConsoleEventHandler.cs
@@ -1,44 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using kCura.EventHandler;
+using kCura.EventHandler.CustomAttributes;
 using kCura.Relativity.Client;
 using Relativity.API;
+using Relativity.Services.Objects;
 
-namespace $safeprojectname$
+namespace Relativity.ConsoleEventHandler
 {
-	[kCura.EventHandler.CustomAttributes.Description("Console EventHandler")]
-	[System.Runtime.InteropServices.Guid("$guid1$")]
-	public class ConsoleEventHandler : kCura.EventHandler.ConsoleEventHandler
-	{
-		public override kCura.EventHandler.Console GetConsole(kCura.EventHandler.ConsoleEventHandler.PageEvent pageEvent)
-		{
-			kCura.EventHandler.Console returnConsole = new kCura.EventHandler.Console();
-			return returnConsole;
-		}
+    [kCura.EventHandler.CustomAttributes.Description("Console EventHandler")]
+    [System.Runtime.InteropServices.Guid("8232e1e6-8db6-4bd2-bd88-384e6afe2cf1")]
+    public class ConsoleEventHandler : kCura.EventHandler.ConsoleEventHandler
+    {
 
-		public override void OnButtonClick(kCura.EventHandler.ConsoleButton consoleButton)
-		{
-			switch (consoleButton.Name)
-			{
-				//Handle each Button's functionality
-			}
-		}
+        public override kCura.EventHandler.Console GetConsole(PageEvent pageEvent)
+        {
+            kCura.EventHandler.Console returnConsole = new kCura.EventHandler.Console();
 
-		/// <summary>
-    /// The RequiredFields property tells Relativity that your event handler needs to have access to specific fields that you return in this collection property
-    /// regardless if they are on the current layout or not. These fields will be returned in the ActiveArtifact.Fields collection just like other fields that are on
-    /// the current layout when the event handler is executed.
-    /// </summary>
-		public override kCura.EventHandler.FieldCollection RequiredFields
-		{
-			get
-			{
-				kCura.EventHandler.FieldCollection retVal = new kCura.EventHandler.FieldCollection();
-				return retVal;
-			}
-		}
-	}
+            Int32 currentWorkspaceArtifactID = Helper.GetActiveCaseID();
+
+            //The Object Manager is the newest and preferred way to interact with Relativity instead of the Relativity Services API(RSAPI). 
+            //The RSAPI will be scheduled for depreciation after the Object Manager reaches feature party with it.
+            using (IObjectManager objectManager = this.Helper.GetServicesManager().CreateProxy<IObjectManager>(ExecutionIdentity.System))
+            {
+
+            }
+
+            //Setting up an RSAPI Client
+            using (IRSAPIClient proxy =
+                Helper.GetServicesManager().CreateProxy<IRSAPIClient>(ExecutionIdentity.System))
+            {
+                //Set the proxy to use the current workspace
+                proxy.APIOptions.WorkspaceID = currentWorkspaceArtifactID;
+                //Add code for working with RSAPIClient
+            }
+
+            Relativity.API.IDBContext workspaceContext = Helper.GetDBContext(currentWorkspaceArtifactID);
+            //Start the transaction
+            workspaceContext.BeginTransaction();
+
+            //Get a dbContext for the EDDS database
+            Relativity.API.IDBContext eddsDBContext = Helper.GetDBContext(-1);
+
+            IAPILog logger = Helper.GetLoggerFactory().GetLogger();
+            logger.LogVerbose("Log information throughout execution.");
+
+            return returnConsole;
+        }
+
+        public override void OnButtonClick(ConsoleButton consoleButton)
+        {
+            switch (consoleButton.Name)
+            {
+                //Handle each Button's functionality
+            }
+        }
+
+        /// <summary>
+        ///     The RequiredFields property tells Relativity that your event handler needs to have access to specific fields that
+        ///     you return in this collection property
+        ///     regardless if they are on the current layout or not. These fields will be returned in the ActiveArtifact.Fields
+        ///     collection just like other fields that are on
+        ///     the current layout when the event handler is executed.
+        /// </summary>
+        public override FieldCollection RequiredFields
+        {
+            get
+            {
+                kCura.EventHandler.FieldCollection retVal = new kCura.EventHandler.FieldCollection();
+                return retVal;
+            }
+        }
+    }
 }
