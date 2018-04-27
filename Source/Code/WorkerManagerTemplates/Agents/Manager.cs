101e7fe1557c11d96cac9d0ad852a6b37e302a7d
Marlon Smith <msmith@kcura.com>
2018-04-27 15:42:33 -0500

    organized solution

---
+++ Source/Code/WorkerManagerTemplates/Agents/Manager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Relativity.API;
+using Helpers;
+
+namespace Agents
+{
+	// Change the name and guid for your application
+	[kCura.Agent.CustomAttributes.Name("Manager Agent Template")]
+	[System.Runtime.InteropServices.Guid("7E42DCAB-2828-4CAC-8CDF-2DD18F172A6D")]
+	public class Manager : kCura.Agent.AgentBase
+	{
+		private IAPILog Logger;
+
+		public override void Execute()
+		{
+			ExecuteAsync().Wait();
+		}
+
+		public async Task ExecuteAsync()
+		{
+		    Helpers.IQuery queryHelper = new Query();
+			IEnumerable<Int32> resourceGroupIds = GetResourceGroupIDs();
+			Logger = Helper.GetLoggerFactory().GetLogger();
+		    ManagerJob job = new ManagerJob(AgentID, Helper, queryHelper, DateTime.Now, resourceGroupIds, Logger);
+			job.OnMessage += MessageRaised;
+
+			try
+			{
+				RaiseMessage("Enter Agent", 10);
+				await job.ExecuteAsync();
+				RaiseMessage("Exit Agent", 10);
+			}
+			catch (Exception ex)
+			{
+				//Raise an error on the agents tab and event viewer
+				RaiseError(ex.ToString(), ex.ToString());
+				Logger.LogError(ex, String.Format("{0} - {1}", Constant.Names.ApplicationName, ex));
+
+				//Add the error to our custom Errors table
+				queryHelper.InsertRowIntoErrorLogAsync(Helper.GetDBContext(-1), job.WorkspaceArtifactId, Constant.Tables.ManagerQueue, job.RecordId, job.AgentId, ex.ToString()).Wait();
+
+				//Add the error to the Relativity Errors tab
+				//this second try catch is in case we have a problem connecting to the RSAPI
+				try
+				{
+					Helpers.Rsapi.ErrorQueries.WriteError(Helper.GetServicesManager(), ExecutionIdentity.System, job.WorkspaceArtifactId, ex);
+				}
+				catch (Exception rsapiException)
+				{
+					RaiseError(rsapiException.ToString(), rsapiException.ToString());
+					Logger.LogError(rsapiException, String.Format("{0} - {1}", Constant.Names.ApplicationName, rsapiException));
+				}
+
+				//Set the status in the queue to error
+				queryHelper.UpdateStatusInManagerQueueAsync(Helper.GetDBContext(-1), Constant.QueueStatus.Error, job.RecordId).Wait();
+			}
+		}
+
+		public override String Name
+		{
+			get { return "Manager Agent Template"; }
+		}
+
+		private void MessageRaised(Object sender, String message)
+		{
+			RaiseMessage(message, 10);
+		}
+	}
+}
---
+++ Source/Code/WorkerManagerTemplates/CustomPages/js/workerAgent[index]-v1.0.js
@@ -0,0 +1,17 @@
+﻿(function (nm, $) {
+    nm.init = function () {
+        $.ajaxSetup({
+            cache: false,
+            error: function (xhr) {
+                $('#modal').removeClass('loading');
+                $('#dvIndex').html(xhr.responseText);
+            }
+        });    
+    };
+})(window.WorkerAgent = window.WorkerAgent || {}, jQuery);
+
+$(document).ajaxStart(function () {
+    $('#modal').addClass('loading');
+}).ajaxStop(function () {
+    $('#modal').removeClass('loading');
+});
