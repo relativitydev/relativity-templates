b2ca61c530e9de6c4a0337333d7c45e2b5ca680d
Thomas Lachin <thomas.lachin@relativity.com>
2019-10-17 19:00:37 -0500

    Still broken in some regards, but meh

--- Source/VSIX/RelativityWizard/InputFormService.Designer.cs
+++ Source/VSIX/RelativityWizard/InputFormService.Designer.cs
@@ -41,7 +41,7 @@
 			// 
 			// pictureBox1
 			// 
-			//this.pictureBox1.Image = global::RelativityWizard.Properties.Resources.platform_stacked;
+			this.pictureBox1.Image = global::RelativityWizard.Properties.Resource.platform_stacked;
 			this.pictureBox1.Location = new System.Drawing.Point(-5, -24);
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.Size = new System.Drawing.Size(494, 197);
