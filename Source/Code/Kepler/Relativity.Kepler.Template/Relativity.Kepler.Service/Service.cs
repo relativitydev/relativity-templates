a80913c5f6a3c39ecbf903949e3f8505a48d71b4
Thomas Lachin <thomas.lachin@relativity.com>
2019-10-25 13:57:53 -0500

    Got it mostly working

--- Source/Code/Kepler/Relativity.Kepler.Template/Relativity.Kepler.Service/Service.cs
+++ Source/Code/Kepler/Relativity.Kepler.Template/Relativity.Kepler.Service/Service.cs
@@ -9,7 +9,10 @@ namespace $rootnamespace$
 {
     public class $safeitemrootname$ : I$safeitemrootname$
     {
-        public $safeitemrootname$(IHelper helper, ILog logger)
+				private IHelper _helper;
+				private ILog _logger;
+
+				public $safeitemrootname$(IHelper helper, ILog logger)
         {
             _logger = logger.ForContext <$safeitemrootname$>();
             _helper = helper;
