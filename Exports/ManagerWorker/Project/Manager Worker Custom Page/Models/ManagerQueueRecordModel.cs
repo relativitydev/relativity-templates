46dddc466d4158435cfa3f32742480b962a61871
Priyanka Shahani <priyanka.pande@relativity.com>
2021-10-08 16:47:16 -0500

    "REL-575925 | Checking in extension code for 4.2

--- Exports/ManagerWorker/Project/Manager Worker Custom Page/Models/ManagerQueueRecordModel.cs
+++ Exports/ManagerWorker/Project/Manager Worker Custom Page/Models/ManagerQueueRecordModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Threading.Tasks;
-using $saferootprojectname$.Helpers;
+using Helpers;
 
 namespace $safeprojectname$.Models
 {
